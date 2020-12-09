393da099bb262877085613b36d9cfa043dda2d71
sahin88 <sahinmuratogur@gmail.com>
2020-12-09 05:00:19 +0100

    Fix bugs

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
